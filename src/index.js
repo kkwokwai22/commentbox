978bcaf3b76b3cb7a685ac2a0a64f415df038a35
Kevin Wong <kkwokwai22@gmail.com>
2017-01-21 13:32:39 -0800

    able to render react in dom and set up express

--- src/index.js
+++ src/index.js
@@ -8,12 +8,24 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentBox from './CommentBox';
-import style from './style';
+
+class CommentBox extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+};
+
 
 ReactDOM.render(
  <CommentBox />,
- document.getElementById(‘root’)
+ document.getElementById('root')
 ); 
 
 
